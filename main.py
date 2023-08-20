1b300b7fb2ee87d8ae42350b3776c4a27e5038f6
Klown <klowninabout@gmail.com>
2023-08-20 16:10:33 -0400

    base game complete

--- main.py
+++ main.py
@@ -1,16 +1,28 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+import random
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+def roll_dice(num_dice):
+    rolls = []
+    for i in range(num_dice):
+        rolls += [random.randint(1, 6)]
+    return print(str(rolls)[1:-1])
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+def main():
+    while True:
+        try:
+            num_dice = input("How many dice would you like to roll?\n")
+            num_dice = num_dice.lower()
+            if num_dice == 'exit':
+                print("Thanks for playing!")
+                break
+            num_dice = int(num_dice)
+            if num_dice == 0:
+                raise ValueError
+        except ValueError:
+            print("Please input a valid number.")
+            continue
+        roll_dice(num_dice)
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+main()
